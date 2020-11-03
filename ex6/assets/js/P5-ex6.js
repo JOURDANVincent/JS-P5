ed10823048afc7b542fa7797161bd4e2e6f037d0
Lendesz <lendesz@gmail.com>
2020-11-03 16:20:16 +0100

    maj

---
+++ ex6/assets/js/P5-ex6.js
@@ -0,0 +1,8 @@
+let iName = document.getElementsById("name");
+let iMail = document.getElementsById("mail");
+let iAge = document.getElementsById("age");
+
+focusI1 = function getFocus() {          
+    document.getElementById("name").focus();
+    document.style.border
+  }
