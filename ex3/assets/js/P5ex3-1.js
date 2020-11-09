a10c58c5809a8f646d1e0e3e6801d78fbc665168
Lendesz <lendesz@gmail.com>
2020-11-09 11:34:04 +0100

    maj ex3 - use onkeyup and match

---
+++ ex3/assets/js/P5ex3-1.js
@@ -0,0 +1,64 @@
+let iName = document.getElementById("name");
+let mName = document.getElementById("missName");
+let regName = /^[A-Za-z\-]+$/;
+
+let iMail = document.getElementById("mail");
+let mMail = document.getElementById("missMail");
+let regMail =/^[A-Za-z]+@[A-Za-z\-]+\.{1}[a-z]{2,5}$/;
+
+let iAge = document.getElementById("age");
+let mAge = document.getElementById("missAge");
+let regAge = /^[0-9]{1,4}$/;
+
+// Test input Name !! //
+iName.onkeyup = function () {
+    let resName = iName.value.match(regName);
+    if (iName.validity.valueMissing) {
+        mName.textContent = "Champ obligatoire !!";
+        iName.style.outlineColor = iName.style.backgroundColor = "red";
+    } else if (resName == null) {
+        mName.textContent = "Ne doit contenir que des lettres !!";
+        iName.style.outlineColor = iName.style.backgroundColor = "red";
+    } else {
+        mName.textContent = "BRAVO !!!";
+        iName.style.outlineColor = iName.style.backgroundColor = "green";
+    }
+}
+
+// Test input Mail !! //
+iMail.onkeyup = function () {
+    let resMail = iMail.value.match(regMail);
+    console.log(resMail);
+    if (iMail.validity.valueMissing) {
+        mMail.textContent = "Champ obligatoire !!";
+        iMail.style.outlineColor = iMail.style.backgroundColor = "red";
+    } else if (resMail == null) {
+        mMail.textContent = "Doit être sous forme abc@def.gh !!";
+        iMail.style.outlineColor = iMail.style.backgroundColor = "red";
+    } else {
+        mMail.textContent = "BRAVO !!!";
+        iMail.style.outlineColor = iMail.style.backgroundColor = "green";
+    }
+}
+
+// Test input Age !! //
+iAge.onkeyup = function () {
+    let resAge = iAge.value.match(regAge);
+    console.log(resAge);
+    if (iAge.validity.valueMissing) {
+        mAge.textContent = "Champ obligatoire !!";
+        iAge.style.outlineColor = iAge.style.backgroundColor = "red";
+    } else if (iAge.value.length >3) {
+        mAge.textContent = "Ne doit contenir que 3 caratères !!";
+        iAge.style.outlineColor = iAge.style.backgroundColor = "orange";
+    } else if (iAge.value <= 0 | iAge.value >= 130) {
+        mAge.textContent = "Ton age doit être compris entre 1 et 130 !!";
+        iAge.style.outlineColor = iAge.style.backgroundColor = "yellow";
+    } else if (resAge == null) {
+        mAge.textContent = "Ne doit contenir que des chiffes !!";
+        iAge.style.outlineColor = iAge.style.outlineColor = iAge.style.backgroundColor = "red";
+    } else {
+        mAge.textContent = "BRAVO !!!";
+        iAge.style.outlineColor = iAge.style.outlineColor = iAge.style.backgroundColor = "green";
+    }
+}
--- ex3/assets/js/P5ex3.js
+++ ex3/assets/js/P5ex3.js
@@ -4,11 +4,11 @@ let vName = new RegExp(/^[A-Za-z\é\è\ê\ï\-]+$/);
 
 let iMail = document.getElementById("mail");
 let missMail = document.getElementById("missMail");
-let vMail = new RegExp(/^[A-Za-z\é\è\ê\ï]+@[A-Za-z\é\è\ê\ï]+\.{1}fr/);
+let vMail = new RegExp(/^[A-Za-z0-9]+@[A-Za-z\é\è\ê\-]+\.{1}fr$/);
 
 let iAge = document.getElementById("age");
 let missAge = document.getElementById("missAge");
-let vAge = new RegExp(/^[0-9]{,3}/);
+let vAge = new RegExp(/^[0-9]{1,3}$/);
 
 iName.addEventListener('input', checkName);
 iMail.addEventListener('input', checkMail);
@@ -18,12 +18,12 @@ function checkName(){
     if (iName.validity.valueMissing) {
         missName.textContent = 'Champ obligatoire';
         missName.style.backgroundColor = "red";
-    } else if (vName.test(iName.value) == false) {
-        missName.textContent = 'Ce champ ne doit contenir que des lettres ...!';
-        missName.style.backgroundColor = "red";
     } else if (iName.value.length < 3){
         missName.textContent = 'Ce champ doit contenir 3 caractères minimum ...!';
         missName.style.backgroundColor = "orange";
+    } else if (vName.test(iName.value) == false) {
+        missName.textContent = 'Ce champ ne doit contenir que des lettres ...!';
+        missName.style.backgroundColor = "red";
     } else {
         missName.textContent = 'Correct';
         missName.style.backgroundColor = "green";
@@ -35,7 +35,7 @@ function checkMail(){
         missMail.textContent = 'Champ obligatoire';
         missMail.style.backgroundColor = "red";
     } else if (vMail.test(iMail.value) == false) {
-        missMail.textContent = 'Ce champ ne doit contenir : abc@cde.fr'
+        missMail.textContent = 'Ce champ doit contenir élément du type: abc@cde.fr'
         missMail.style.backgroundColor = "red";
     }
     else {
@@ -48,21 +48,15 @@ function checkAge(){
     if (iAge.validity.valueMissing) {
         missAge.textContent = 'Champ obligatoire';
         missAge.style.backgroundColor = "red";
-    }
-    else if (vAge.test(iAge.value) == false) {
-        if (iAge.value.length > 3) {
-            missAge.textContent = '3 caractères maximumu autorisé !!!'
-            missAge.style.backgroundColor = "orange";
-        } else if (iAge.value > 0){
-            missAge.textContent = ''
-            missAge.style.backgroundColor = "none";
-        } else {
-            missAge.textContent = 'Ce champ ne doit contenir que des chiffres'
-            missAge.style.backgroundColor = "red";
-        }
     } else if(iAge.value <= 0 | iAge.value >= 130){
         missAge.textContent = "Ton age doit être compris entre 0 et 130";
         missAge.style.backgroundColor = "yellow";
+    } else if (iAge.value.length > 3) {
+        missAge.textContent = '3 caractères maximumu autorisé !!!'
+        missAge.style.backgroundColor = "orange";
+    } else if (vAge.test(iAge.value) == false) {
+        missAge.textContent = 'Ce champ ne doit contenir que des chiffres'
+        missAge.style.backgroundColor = "red";
     }
     else {
         missAge.style.backgroundColor = "green";
