a2b2cd6cd70466835be19452bfd41849583730a3
Lendesz <lendesz@gmail.com>
2020-11-12 16:35:00 +0100

    maj ex6 et ex8

--- ex6/assets/js/P5-ex6.js
+++ ex6/assets/js/P5-ex6.js
@@ -4,14 +4,12 @@ iPink.addEventListener('click', pinkFocus);
 let iBlue = document.getElementById("bluename");
 iBlue.addEventListener('click', blueFocus);
 
-
 function pinkFocus() {
   iPink.style.outlineColor = "red";
   iBlue.style.border = "none";
   iPink.focus();
   iBlue.blur();
 }  
-
 
 function blueFocus() {
   iBlue.style.outlineColor = "blue";
--- ex8/assets/js/P5-ex8.js
+++ ex8/assets/js/P5-ex8.js
@@ -1,11 +1,14 @@
+let i = 0;
+
 function cloneDiv() {
+    i++;
     cloneFirstName = document.getElementById("firstName").cloneNode(true);
     cloneAge = document.getElementById("age").cloneNode(true);
+    cloneFirstName.id += i;
+    document.getElementById('form').appendChild (cloneFirstName);
+    cloneAge.id += i;
+    document.getElementById('form').appendChild (cloneAge);
 
-    cloneFirstName.id = "firstName2"
-    document.getElementById("form").appendChild (cloneFirstName);
-    cloneFirstName.id = "age2"
-    document.getElementById("form").appendChild (cloneAge);
 }
 
 
